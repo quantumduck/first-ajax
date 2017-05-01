5410cfdad2e004a1187848a7286039ac12186bd6
quantumduck <quantum.duck@hotmail.com>
2017-05-01 12:11:25 -0400

    steps 1 and 2 done, but no data in response

--- ajax.js
+++ ajax.js
@@ -1,5 +1,17 @@
 $(document).ready(function () {
-
-  /* Your code goes here */
+  var rootURL = 'http://first-ajax-api.herokuapp.com/';
+  $('#step12').on('click', function() {
+    $.ajax({
+      url: rootURL,
+      method: 'GET',
+      dataType: 'html'
+      // data: { limit: 2, stuff: 4 }
+    }).done(function(data) {
+      $('#step12').append(data);
+      console.log('got' + data)
+    }).fail(function() {}).always(function() {
+      console.log('First!');
+    });
+  });
 
 });
