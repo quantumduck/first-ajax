76c459a57996f9eaada958353d45547b896bd3ad
quantumduck <quantum.duck@hotmail.com>
2017-05-01 12:37:35 -0400

    added counter for step 7

--- ajax.js
+++ ajax.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     $.ajax({
       url: rootURL,
       method: 'GET',
-      dataType: 'html'
+      dataType: 'text'
       // data: { limit: 2, stuff: 4 }
     }).done(function(data) {
       $('#step12').append(data);
@@ -18,7 +18,7 @@ $(document).ready(function () {
     $.ajax({
       url: rootURL + pingPong(),
       method: 'GET',
-      dataType: 'html'
+      dataType: 'text'
       // data: { limit: 2, stuff: 4 }
     }).done(function(data) {
       $('#step3456').append(data);
@@ -27,6 +27,21 @@ $(document).ready(function () {
       $('#step3456').append('FAILURE: Your code is bad, and you should feel bad!');
     }).always(function() {
       $('#step3456').append('<br>Thank you for your kind request.<br>');
+    });
+  });
+
+  $('#step7').on('click', function() {
+    $.ajax({
+      url: rootURL + 'count',
+      method: 'GET',
+      dataType: 'text'
+      // data: { limit: 2, stuff: 4 }
+    }).done(function(num) {
+      $('#step7').append('Congratulations! You are the ' + num + suffix(num) + ' visitor!');
+    }).fail(function() {
+      $('#step7').append('FAILURE: Your code is bad, and you should feel bad!');
+    }).always(function() {
+      $('#step7').append('<br>Thank you for your kind request.<br>');
     });
   });
 
@@ -40,5 +55,19 @@ function pingPong() {
   } else {
     runNum++;
     return 'pong';
+  }
+}
+
+function suffix(num) {
+  switch (num % 100) {
+    case 11: return 'th';
+    case 12: return 'th';
+    case 13: return 'th';
+    default: switch (num % 10) {
+      case 1: return 'st';
+      case 2: return 'nd';
+      case 3: return 'rd';
+      default: return 'th';
+    }
   }
 }
